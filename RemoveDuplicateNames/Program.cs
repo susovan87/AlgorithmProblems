da3d183a53b11a9654eabf314ecd48d2a597b33d
Susovan Ghosh <susovan87@gmail.com>
2012-11-21 00:17:28 +0530

    first check in

---
+++ RemoveDuplicateNames/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveDuplicateNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names;
+            do
+            {
+                Console.Write("Please enter a list of names (separated by space):");
+                names = Console.ReadLine();
+                try
+                {
+                    List<string> lstNames = RemoveDuplicates(names.Split(new char[] {' '}).ToList());
+                    Console.WriteLine(string.Join(", ",lstNames));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (names.Length > 0);
+        }
+
+        static List<string> RemoveDuplicates(List<string> names)
+        {
+            List<string> lstNames = new List<string>();
+            string strNames = string.Join(",", names) + ",";
+
+            foreach(string name in names)
+            {
+                if (strNames.Length == 0)
+                    break;
+
+                if (strNames.Contains(name + ","))
+                {
+                    strNames = strNames.Replace(name + ",", "");
+                    lstNames.Add(name);
+                }
+            }
+
+            return lstNames;
+        }
+    }
+}
---
+++ SingleDigitProduct/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingleDigitProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+            do
+            {
+                Console.Write("Please enter a number:");
+                num = Console.ReadLine();
+                try
+                {
+                    int result = GetSingleDigitProduct(Convert.ToInt64(num));
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }while(num.Length>0);
+        }
+
+        static int GetSingleDigitProduct(long n)
+        {
+            int iteration=0;
+            while(n>=10)
+            {
+                long result = 1;
+                iteration++;
+                foreach (char c in n.ToString())
+                {
+                    result *= Convert.ToInt32(c.ToString());
+                }
+                n = result;
+            }
+            return iteration;
+        }
+    }
+}
